b1eb1ca5740e6af303be0d1d661fb93e6333cfae
Falcone <jose.ismael@dcx.ufpb.br>
2020-07-07 17:06:36 -0300

    editUser

--- springboot/src/main/java/pos/controller/UserController.java
+++ springboot/src/main/java/pos/controller/UserController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.*;
-=======
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> 5997d1c8e6a31ad82d2c401406c2fb3a5ba30f3b
+
 
 import pos.dto.UserDTO;
 import pos.model.User;
